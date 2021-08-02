42d6cbd3992abbce4b84f597d3a957b7a2e2a3f2
周进 <zhoujin@zhoujindeMacBook-Pro.local>
2021-08-02 21:23:01 +0800

    sub-vue sub-react基本配置

--- sub-react/src/App.js
+++ sub-react/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { Divider } from "antd";
+import 'antd/dist/antd.min.css';
 import './App.css';
+import Home from './pages/Home';
 
-function App() {
+const About = lazy(() => import('./pages/About'));
+
+const RouteExample = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // basename 必须与基座 base 保持一致 TODO: 可以封装一个组件来统一解决这个问题
+    <Router basename={window.__POWERED_BY_QIANKUN__ ? '/react' : '/'}>
+      <nav>
+        <Link to="/">Home</Link>
+        <Divider type="vertical" />
+        <Link to="/about">About</Link>
+      </nav>
+      <Suspense fallback={null}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
+export default function App() {
+  return (
+    <div className="app-main">
+      <RouteExample></RouteExample>
     </div>
   );
 }
-
-export default App;
